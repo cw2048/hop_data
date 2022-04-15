a805bb45b56f5bc8a0a7d37fac0671e066f4af5e
cw2048 <chui@nevada.unr.edu>
2022-04-15 13:39:26 -0700

    working on add

--- new_hop_data/add_birds.py
+++ new_hop_data/add_birds.py
@@ -6,13 +6,14 @@ import sys
 import re
 
 for file in sys.argv[1:]:
-    IN = open(file, 'r')
-	match = re.search("(\d+)_(\w+)(\d+), (\d+)", file)
-	#date = [match.group(2), match.group(1), match.group(3)]
-	#CTR = 0
-	#time = 0
-	#for Line in IN:
-		#if re.search("^\d+ ", Line):
+	IN = open(file, 'r')
+	match = re.search("(\d+)_(\w+) (\d+), (\d+)", file)
+	date = [match.group(2), match.group(1), match.group(3)]
+	CTR = 0
+	time = 0
+	#OUT=open("out.txt", 'w')
+	for Line in IN:
+		if re.search("^\d+ ", Line):
 		#Line = Line.strip("\n")
 		#CTR =+ 1
 		#time =+ 1
@@ -21,5 +22,6 @@ for file in sys.argv[1:]:
 		#OUT.write("%d\t" % (time))
 		#OUT.write("1\t")
 		#OUT.write(Line + "\n")
+	#print(date)
 	#IN.close()
 #OUT.close()
